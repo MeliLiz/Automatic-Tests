3fb2ce1f90d080c31bf18d848f97535c097846d2
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-09 17:09:24 -0600

    Getting results with duck duck go and selecting the first result

--- PruebasAutomaticas.py
+++ PruebasAutomaticas.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 # Create a new instance of the Chrome driver
 driver = webdriver.Chrome()
 
+"""
 # Navigate to the application home page
 driver.get('https://www.google.com')
 
@@ -20,3 +21,20 @@ for i, result in enumerate(results):
     
 # Close the browser
 driver.quit()
+"""
+# With DuckDuckGo
+driver.get('https://start.duckduckgo.com')
+search_box = driver.find_element(By.ID, 'searchbox_input')
+search_box.send_keys('Python')
+search_box.submit()
+
+# Find the results
+results = driver.find_elements(By.CSS_SELECTOR, 'h3')
+for i, result in enumerate(results):
+    print(f'{i + 1}. {result.text}')
+    
+# Select the first result
+sel = driver.find_element(By.XPATH, '//*[@id="r1-0"]/div/h2/a[1]')
+sel.click()
+print(driver.current_url)
+
