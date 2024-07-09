8da1238bc28a6e721f0940c8124a9e9d78a9e9b4
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-09 17:02:22 -0600

    Enumerando los resultados

--- PruebasAutomaticas.py
+++ PruebasAutomaticas.py
@@ -11,6 +11,12 @@ driver.get('https://www.google.com')
 search_box = driver.find_element(By.CSS_SELECTOR, '[title="Buscar"]')
 search_box.send_keys('Python')
 search_box.submit()
-"""text_box = driver.find_element(By.NAME, 'q')
-text_box.send_keys('Python')
-text_box.submit()"""
+
+
+# Enumerar los resultados de la búsqueda
+results = driver.find_elements(By.CSS_SELECTOR, 'h3')
+for i, result in enumerate(results):
+    print(f'{i + 1}. {result.text}')
+    
+# Cerrar el navegador
+driver.quit()
