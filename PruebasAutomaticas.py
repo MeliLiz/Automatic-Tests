9135cb360f2a54239aab428be9856fe2c1c06ea2
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-09 17:03:14 -0600

    Comments

--- PruebasAutomaticas.py
+++ PruebasAutomaticas.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Se crea una instancia de Chrome
+# Create a new instance of the Chrome driver
 driver = webdriver.Chrome()
 
-# Se navega a la página de Google
+# Navigate to the application home page
 driver.get('https://www.google.com')
 
-
+# Find the search box
 search_box = driver.find_element(By.CSS_SELECTOR, '[title="Buscar"]')
 search_box.send_keys('Python')
 search_box.submit()
 
 
-# Enumerar los resultados de la búsqueda
+# Find the results
 results = driver.find_elements(By.CSS_SELECTOR, 'h3')
 for i, result in enumerate(results):
     print(f'{i + 1}. {result.text}')
     
-# Cerrar el navegador
+# Close the browser
 driver.quit()
