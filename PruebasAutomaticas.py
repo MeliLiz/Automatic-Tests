971e2d165a02d1dc3f2dc8cc28809c18e043a877
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-09 17:55:08 -0600

    Finding the length of the type grass pokemons

--- PruebasAutomaticas.py
+++ PruebasAutomaticas.py
@@ -52,12 +52,16 @@ for row in rows:
     
 """
 driver.get("https://pokemondb.net/pokedex/stats/gen1")
+table = driver.find_element(By.CLASS_NAME, 'data-table') # Find the table
+body = table.find_element(By.TAG_NAME, 'tbody') # Find the body
+grass_type = body.find_elements(By.Class_NAME, 'type-grass') # Find the rows
+print(len(grass_type))
 # Search the type grass and get the results
-type_filter = driver.find_element(By.XPATH, '//*[@id="filter-pkmn-type"]')
-#type_filter = driver.find_element(By.ID, 'filter-pkmn-type')
+#type_filter = driver.find_element(By.XPATH, '//*[@id="filter-pkmn-type"]')
 # Select the type grass
-type_filter.click()
-driver.find_element(By.XPATH, '//option[text()="Grass"]').click()
+#type_filter.click()
+#driver.find_element(By.XPATH, '//option[text()="Grass"]').click()
+
 
 
 
