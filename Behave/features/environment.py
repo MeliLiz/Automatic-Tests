9ccd36832bdf97591db9da0df3682ee1ac9f48c9
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-11 14:27:47 -0600

    Open chrome

--- Behave/features/environment.py
+++ Behave/features/environment.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from configparser import ConfigParser
 from unittest import TestCase
+from helper.getBrowser import get_browser
 
 # This will run before all scenarios
 def before_all(context):
@@ -10,10 +11,8 @@ def before_all(context):
     config.read('setup.cfg')
     context.config = config # Save the config object to the context
 
-    # Create a new instance of the Chrome driver
-    driver = webdriver.Chrome()
-    driver.get("https://pokemondb.net/pokedex/stats/gen1")
-    context.driver = driver # Save the driver object to the context
+    context.driver = get_browser(context.config.get('Browser', 'navegador')) # Save the driver object to the context
+    context.driver.set_window_size(context.config.get('Browser', 'width'), context.config.get('Browser', 'height')) # Set the window size
     
     """table = driver.find_element(By.CLASS_NAME, 'data-table') # Find the table
     body = table.find_element(By.TAG_NAME, 'tbody') # Find the body
