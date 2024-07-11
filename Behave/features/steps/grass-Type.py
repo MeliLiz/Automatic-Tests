4fd924d6762813765801589112ed1dfac2b0b157
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-11 14:10:51 -0600

    structure to create the tests

---
+++ Behave/features/steps/grass-Type.py
@@ -0,0 +1,12 @@
+
+@given(u'Validar la cantidad de pokemon de la primera generación')
+def step_impl(context):
+    pass
+
+@when(u'Se accede a la página de pokedex')
+def step_impl(context):
+    pass
+
+@then(u'Validar que existan 151 pokemon')
+def step_impl(context):
+    pass
