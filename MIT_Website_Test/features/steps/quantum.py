8c58e6370e280728b12a1e66d120aa488378241a
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-11 18:29:32 -0600

    Trying to get the page

--- MIT_Website_Test/features/steps/quantum.py
+++ MIT_Website_Test/features/steps/quantum.py
@@ -20,23 +20,28 @@ def search_for_quantum(context, search_term):
         search_box.send_keys(search_term + Keys.RETURN)
         print(f"Searching for {search_term}")
     except Exception as e:
-        print(f"Error finding the search box: {e}")
-    
+        print("Error finding the search box: {e}")
     # Wait for the search results to be displayed
     search_results = WebDriverWait(context.driver, 10).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a")) # Waits for all the links to be present <a>
     )
-
-    # Extract the links from the search results
-    for result in search_results:
-        link = result.get_attribute('href')
-        if link:
-            print(link)
+    print(f"Found {len(search_results)} search results")
+    context.test.assertTrue(len(search_results) > 0)
+    # context.test.assertEqual(len(search_results), 69300)
+    
     
 @then('Verify the results')
-def verify_results(context):
-    pass
-    """results = context.driver.find_elements(By.XPATH, '//h3')
-    for result in results:
-        assert 'quantum' in result.text.lower()"""
+def verify_results(context): # Verify if there is a page that includes 'A new way for quantum computing systems to keep their cool'
+    # Find all the search results
+    search_results = context.driver.find_elements(By.TAG_NAME, "h3")
+    result_ = None
+    for result in search_results:
+        print(result.text)
+        if result.text == "A new way for quantum computing systems to keep their cool":
+            result_ = result
+            result.click()
+            break
+    context.test.assertIsNotNone(result_)
+
+       
         
